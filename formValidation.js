c70b3d7f03ef13168b3ca219489b69bf14b3239e
jd90 <1418146@rgu.ac.uk>
2016-02-19 14:08:31 +0000

    s

---
+++ formValidation.js
@@ -0,0 +1,35 @@
+function validate(form) {
+
+    isValid = true;
+    message = "These are required fields: \n";
+    message2 = "\n Please correct before submitting.";
+    errors = "";
+
+    if (form.address.value == "") {
+        isValid = false;
+        errors += " address \n";
+    }
+
+    if (form.image.value == "") {
+        isValid = false;
+        errors += " image link \n";
+    }
+
+    if (form.description.value == "") {
+        isValid = false;
+        errors += " description \n";
+    }
+    if (form.bbName.value == "") {
+        isValid = false;
+        errors += "News summary required \n";
+    }
+
+    if (isValid) {
+        return true;
+    } else {
+        alert(message + errors +message2);
+        return false;
+    }
+
+
+}
