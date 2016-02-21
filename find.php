5a1ee21e6f915475fbb9c896cace84efbd3692f3
jd90 <1418146@rgu.ac.uk>
2016-02-21 11:20:22 +0000

    s

--- find.php
+++ find.php
@@ -48,7 +48,7 @@ $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         }
         catch(PDOException $e)
         {
-            print"$e->getMessage()";
+            print"$e";
         }
 
 
